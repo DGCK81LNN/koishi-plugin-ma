be55de275b66191f5731d21990f50b5780dd30a7
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2024-04-09 01:18:30 +0800

    feat: send command

--- src/aux_commands.ts
+++ src/aux_commands.ts
@@ -2,34 +2,58 @@ import { Context, Schema, h } from "koishi"
 
 export const name = "ma:aux_commands"
 
+type Toggle<T> = T & { enabled: boolean }
+
+function schemaToggle<T>(schema: Schema<T>, remark: string): Schema<Toggle<T>> {
+  return Schema.intersect([
+    Schema.object({
+      enabled: Schema.boolean().description("启用" + remark),
+    }).description(""),
+    Schema.union([
+      Schema.object({
+        enabled: Schema.const(true).required(true),
+        ...schema.dict,
+      }),
+      Schema.object({
+        enabled: Schema.const(false),
+      }),
+    ]),
+  ]) as Schema<Toggle<T>>
+}
+
+interface CatConfig {
+  fetchMaxLength: number
+  fetchMaxInterpolationLength: number
+}
+
 export interface Config {
-  echo: {
-    enabled: boolean
-  }
-  cat: {
-    enabled: boolean
-    fetchMaxLength: number
-    fetchMaxInterpolationLength: number
-  }
+  echo: Toggle<{}>
+  cat: Toggle<CatConfig>
+  send: Toggle<{}>
 }
 
 export const Config: Schema<Config> = Schema.object({
-  echo: Schema.object({
-    enabled: Schema.boolean().description("启用").default(false),
-  })
-    .collapse()
-    .description("改良的 echo 指令。"),
-  cat: Schema.object({
-    enabled: Schema.boolean().description("启用").default(false),
-    fetchMaxLength: Schema.number()
-      .description("抓取 URL 内容的最大长度。")
-      .default(4000),
-    fetchMaxInterpolationLength: Schema.number()
-      .description("在“$()”插值语法中运行时，抓取 URL 内容的最大长度。")
-      .default(50000),
-  })
-    .collapse()
-    .description("cat 指令。"),
+  echo: schemaToggle(
+    Schema.object({}),
+    "改良版 echo 指令：不会自动将消息元素转换成 XML 形式，并增加更多转义功能。\n\n" +
+      "若要使用，必须停用官方 echo 插件。"
+  ),
+  cat: schemaToggle<CatConfig>(
+    Schema.object({
+      fetchMaxLength: Schema.number()
+        .description("cat 指令抓取 URL 内容的最大长度。")
+        .default(4000),
+      fetchMaxInterpolationLength: Schema.number()
+        .description("cat 指令在“$()”插值语法中运行时，抓取 URL 内容的最大长度。")
+        .default(50000),
+    }),
+    " cat 指令：重复下一条消息，或从 URL 获取文本；同时可进行转义。"
+  ),
+  send: schemaToggle(
+    Schema.object({}),
+    " send 指令：发送消息到指定上下文。\n\n" +
+      "补足了官方 echo 具有但上述改良 echo 没有的功能。"
+  ),
 })
 
 function transform(
@@ -41,13 +65,11 @@ function transform(
   if (markov)
     return h.transform(message, {
       text: ({ content: c }) =>
-        h.text(
-          String(c)
-            .replace(/[\\;=,%]/g, s => "\\" + s)
-            .replace(/\n/g, "\\n")
-            .replace(/\r/g, "\\r")
-            .replace(/\t/g, "\\t")
-        ),
+        String(c)
+          .replace(/[\\;=,%]/g, s => "\\" + s)
+          .replace(/\n/g, "\\n")
+          .replace(/\r/g, "\\r")
+          .replace(/\t/g, "\\t"),
     })
   if (ord)
     return h.transform(message, {
@@ -61,10 +83,18 @@ function transform(
   return message
 }
 
+function parsePlatform(target: string): [platform: string, id: string] {
+  const index = target.lastIndexOf(":")
+  const platform = target.slice(0, index)
+  const id = target.slice(index + 1)
+  console.dir([platform, id])
+  return [platform, id]
+}
+
 export function apply(ctx: Context, config: Config) {
   const logger = ctx.logger(name)
 
-  if (config.echo) {
+  if (config.echo.enabled) {
     ctx
       .command("echo <message:el>", {
         checkUnknown: true,
@@ -76,6 +106,10 @@ export function apply(ctx: Context, config: Config) {
       .option("ord", "-o")
       .option("hexOrd", "-x")
       .action(async ({ session, options }, els) => {
+        if (!els?.length) {
+          await session.send(session.text(".expect-content"))
+          return
+        }
         const { escape, unescape, markov, ord, hexOrd } = options
         if (+!!escape + +!!unescape + +!!markov + +!!ord + +!!hexOrd > 1) {
           await session.send(session.text(".conflicting-options"))
@@ -86,7 +120,7 @@ export function apply(ctx: Context, config: Config) {
       })
   }
 
-  if (config.cat) {
+  if (config.cat.enabled) {
     ctx
       .command("cat [url:rawtext]", {
         checkUnknown: true,
@@ -130,6 +164,38 @@ export function apply(ctx: Context, config: Config) {
           return
         }
         return transform(message, options)
+      })
+  }
+
+  if (config.send.enabled) {
+    ctx
+      .command("send <message:el>", {
+        checkUnknown: true,
+        showWarning: true,
+        authority: 3,
+      })
+      .option("user", "-u <user:user>")
+      .option("channel", "-c <channel:channel>")
+      .option("guild", "-g <guild:string>")
+      .action(async ({ session, options }, content) => {
+        if (!content?.length) return session.send(session.text(".expect-content"))
+
+        // https://github.com/koishijs/koishi/blob/e83e6bd1aabb85e8e415d5eefc0f959d4a4d82fb/plugins/common/echo/src/index.ts#L36-L48
+        const target = options.user || options.channel
+        if (target) {
+          const [platform, id] = parsePlatform(target)
+          const bot = ctx.bots.find(bot => bot.platform === platform)
+          if (!bot) {
+            return session.text(".platform-not-found")
+          } else if (options.user) {
+            await bot.sendPrivateMessage(id, content, session.guildId)
+          } else {
+            await bot.sendMessage(id, content, options.guild)
+          }
+          return
+        }
+
+        return session.text(".expect-context")
       })
   }
 }
